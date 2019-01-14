e8bbaf2f262df7e0522175f8627460118317014f
1600468 <1600468@LABCWS-18.STUDENTS.utech.edu.jm>
2019-01-14 16:31:48 -0500

    ArmBand Machine

---
+++ armband-lib/src/main/java/AP/lib/ArmbandMachine.java
@@ -0,0 +1,14 @@
+package AP.lib;
+
+public interface ArmbandMachine {
+	
+	/***
+	 * Calculates the cost of the arm band
+	 * @param price- cost of an arm band 
+	 * @param quantity - amount of armbands
+	 * 
+	 */
+	float getCost(float price, int quantity);
+	
+
+}
